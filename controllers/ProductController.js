eaca89ec6a49053a90d48d7497b0ea2e9134f75a
Surya Eko <suryaekoindrawan@gmail.com>
2020-10-01 16:36:11 +0700

    [PRODUCT] - detail Product - create Product - update Product - delete Product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,11 +1,61 @@
 // MODELS
 const { Product } = require("../models");
 
+// METHODS
+const ErrorMethod = require("../methods/ErrorMethod");
+
 module.exports = {
   async list(req, res, next) {
     try {
       const listProduct = await Product.findAll();
-      res.send(listProduct);
+      return res.send(listProduct);
+    } catch (err) {
+      return next(err);
+    }
+  },
+  async detail(req, res, next) {
+    try {
+      const productId = req.params.id;
+      const product = await Product.findByPk(productId);
+      if (!product) throw new ErrorMethod.NotFound("Product not found!");
+
+      return res.send(product);
+    } catch (err) {
+      return next(err);
+    }
+  },
+  async create(req, res, next) {
+    try {
+      const product = Product.build(req.body);
+      await product.save();
+
+      return res.send({ success: true });
+    } catch (err) {
+      return next(err);
+    }
+  },
+  async update(req, res, next) {
+    try {
+      const productId = req.params.id;
+      const product = await Product.findByPk(productId);
+      product.set(req.body);
+      await product.save();
+
+      return res.send({ success: true });
+    } catch (err) {
+      return next(err);
+    }
+  },
+  async delete(req, res, next) {
+    try {
+      const productId = req.params.id;
+      await Product.destroy({
+        where: {
+          id: productId,
+        },
+      });
+
+      return res.send({ success: true });
     } catch (err) {
       return next(err);
     }
--- routes/product.js
+++ routes/product.js
@@ -2,4 +2,8 @@ const { ProductController } = require("../controllers");
 
 module.exports = (router) => {
   router.get("/product", ProductController.list);
+  router.get("/product/:id", ProductController.detail);
+  router.post("/product", ProductController.create);
+  router.put("/product/:id", ProductController.update);
+  router.delete("/product/:id", ProductController.delete);
 };
